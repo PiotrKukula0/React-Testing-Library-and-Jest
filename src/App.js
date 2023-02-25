6e01b69351d8dbcf2e8e654fcaf538976f60cdff
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-02-25 20:14:22 +0100

    getAllByRole, getByTestId, getByTitle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 2
+  const b = 4
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +24,8 @@ function App() {
           <li>Banana</li>
           <li>Orange</li>
         </ul>
+        <h1 data-testid="mytestid">Hello</h1>
+        <span title="sum">{a+b}</span>
       </header>
     </div>
   );
